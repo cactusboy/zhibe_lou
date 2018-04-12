7f2abf844f57aafb308885f91c7e2cb93f415320
Zhi-BeYourHero <763378665@qq.com>
2018-04-13 00:49:28 +0800

    first

---
+++ jobplus/models.py
@@ -0,0 +1,42 @@
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from datetime import datetime 
+
+db = SQLAlchemy()
+
+class Base(db.Model):
+    __abstract__ = True
+    created_at = db.Column(db.DateTime, default = datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default = datetime.utcnow, onupdate = datetime.utcnow)
+
+class User(Base, UserMixin):
+    __tablename__ = 'user'
+
+    ROLE_USER = 10
+    ROLE_ENTERPRISE = 20
+    ROLE_ADMIN = 30
+
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(32), unique=True, nullable=True)
+    email = db.Column(db.String(64), unique=True, nullable=True)
+    _password = db.Column('password', db.String(256), nullable=False)
+    role = db.Column(db.SmallInteger, default=ROLE_USER)
+
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, set_password):
+        self._password = generate_password_hash(set_password)
+
+    def check_password(self, password):
+        return check_password_hash(self._password, password)
+    @property
+    def is_enterprise(self):
+        return self.role == self.ROLE_ENTERPRISE
+
+    @property
+    def is_admin(self):
+        return self.role == self.ROLE_ADMIN
